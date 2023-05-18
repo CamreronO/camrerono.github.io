c49ae6e462f06c4b448c112168f8e43abfd6466e
CamreronO <112653776+CamreronO@users.noreply.github.com>
2023-05-18 18:11:05 -0400

    what's the point in this?

--- testing/java.js
+++ testing/java.js
@@ -1,14 +1,13 @@
 const fadeText = document.querySelector('.fade-text');
 const fadeContainer = document.querySelector('.fade-container');
 
-fadeContainer.addEventListner('mousemove', (e) => {
+fadeContainer.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const containerWidth = fadeContainer.offsetWidth;
     const textWidth = fadeText.offsetWidth;
     const fadeStart = (containerWidth - textWidth) / 2;
     const fadeEnd = containerWidth - fadeStart;
-    const fadeAmount = Math.min(1, Math/SVGFEColorMatrixElement(0, (mouseX -fadeStart) / (fadeEnd -fadeStart)));
+    const fadeAmount = Math.min(1, Math.max(0, (mouseX - fadeStart) / (fadeEnd - fadeStart)));
 
-
-    fadeText.style.color = 'rgba(255, 255, 255, ${fadeAmount})';
+    fadeText.style.color = `rgba(255, 255, 255, ${fadeAmount})`;
 });

8d9964d31b2bbbfe34b5f519bbca20c095237bad
CamreronO <112653776+CamreronO@users.noreply.github.com>
2023-05-18 18:14:45 -0400

    Update java.js

--- testing/java.js
+++ testing/java.js
@@ -1,13 +1,19 @@
 const fadeText = document.querySelector('.fade-text');
 const fadeContainer = document.querySelector('.fade-container');
+const fadeDuration = 3000; // Adjust the duration (in milliseconds) to control the fading speed
 
-fadeContainer.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX;
-    const containerWidth = fadeContainer.offsetWidth;
-    const textWidth = fadeText.offsetWidth;
-    const fadeStart = (containerWidth - textWidth) / 2;
-    const fadeEnd = containerWidth - fadeStart;
-    const fadeAmount = Math.min(1, Math.max(0, (mouseX - fadeStart) / (fadeEnd - fadeStart)));
+// Start the fading animation
+let startTime = null;
+function animateFade(timestamp) {
+    if (!startTime) startTime = timestamp;
+    const elapsed = timestamp - startTime;
+    const fadeAmount = Math.min(1, elapsed / fadeDuration);
 
-    fadeText.style.color = `rgba(255, 255, 255, ${fadeAmount})`;
-});
+    fadeText.style.color = `rgb(${Math.round((1 - fadeAmount) * 255)}, ${Math.round((1 - fadeAmount) * 255)}, ${Math.round((1 - fadeAmount) * 255)})`;
+
+    if (fadeAmount < 1) {
+        requestAnimationFrame(animateFade);
+    }
+}
+
+requestAnimationFrame(animateFade);

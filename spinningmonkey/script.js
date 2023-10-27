5de9c7471a4364610854038d9957ddd55ccf6a1d
CamreronO <112653776+CamreronO@users.noreply.github.com>
2023-10-27 14:18:01 -0400

    made an counter

---
+++ spinningmonkey/script.js
@@ -0,0 +1,25 @@
+// Get the necessary elements from the HTML document
+const spinningMonkey = document.getElementById("spinning-monkey");
+const spinCount = document.getElementById("spin-count");
+const resetButton = document.getElementById("reset-button");
+
+let count = 0;
+
+// Function to increment the counter and rotate the monkey
+function spinMonkey() {
+    count++;
+    spinCount.textContent = count;
+    spinningMonkey.style.transform = `rotate(${count * 360}deg)`;
+}
+
+// Reset the counter and monkey rotation
+resetButton.addEventListener("click", () => {
+    count = 0;
+    spinCount.textContent = count;
+    spinningMonkey.style.transform = "rotate(0deg)";
+});
+
+// Start the monkey spinning
+setInterval(spinMonkey, 100);
+
+
